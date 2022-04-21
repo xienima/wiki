d6c9d2c856863330e5d66241a76a216c5c35d847
kddxnm <2055472756@qq.com>
2022-04-21 17:57:19 +0800

    2-6开发HelloWorld接口

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,8 +1,10 @@
-package com.jiawa.wiki;
+package com.jiawa.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.jiawa.wiki")
 @SpringBootApplication
 public class WikiApplication {
 

63117e44d28dae61cfa65d8b79a200345ada0867
xienima <2055472756@qq.com>
2022-07-24 18:56:53 +0800

    3-5 修改数据库为wikidev

--- doc/all.sql
+++ doc/all.sql
@@ -3,6 +3,6 @@ CREATE TABLE `test`  (
                          `id` bigint NOT NULL COMMENT 'id',
                          `name` varchar(50) COMMENT '名称',
                          `password` varchar(50) comment '密码',
-                wiki         PRIMARY KEY (`id`) USING BTREE
+                       PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB default charset= utf8mb4 comment='测试';
 insert into `test`(id,name,password) values (1,'测试','password');

12d3bbad171f6afebce6cb3082de09e37a756137
xienima <2055472756@qq.com>
2022-07-24 19:26:42 +0800

    3-5 集成mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,3 +6,11 @@ CREATE TABLE `test`  (
                        PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB default charset= utf8mb4 comment='测试';
 insert into `test`(id,name,password) values (1,'测试','password');
+
+DROP TABLE IF EXISTS `demo`;
+CREATE TABLE `demo`  (
+                         `id` bigint NOT NULL COMMENT 'id',
+                         `name` varchar(50) COMMENT '名称',
+                         PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB default charset= utf8mb4 comment='测试';
+insert into `demo`(id,name) values (1,'测试');

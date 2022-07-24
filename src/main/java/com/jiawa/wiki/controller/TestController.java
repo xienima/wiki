7d43a423eb6e9f6a3d71f1ab500da869c23bcb0c
xienima <2055472756@qq.com>
2022-07-24 12:40:50 +0800

    2-7 使用HTTP Client测试接口 test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.jiawa.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post,"+ name;
     }
 }

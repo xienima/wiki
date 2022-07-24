40043f0e80e18aa020e7bcf29771e7dbdf0a593c
xienima <2055472756@qq.com>
2022-07-24 13:11:44 +0800

    2-8 SpringBoot配置文件【自定义配置项】

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World "+testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name){

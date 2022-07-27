cc32679ca0b148012ffbd267e517d5ac28320429
xienima <2055472756@qq.com>
2022-07-28 01:01:27 +0800

    6-5 封装分页请求参数PageReq和返回参数PageResp

---
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -0,0 +1,29 @@
+package com.jiawa.wiki.controller;
+
+import com.jiawa.wiki.req.EbookReq;
+import com.jiawa.wiki.resp.CommonResp;
+import com.jiawa.wiki.resp.EbookResp;
+import com.jiawa.wiki.resp.PageResp;
+import com.jiawa.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+
+    @GetMapping("/list")
+    public CommonResp list(EbookReq req){
+        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
+        PageResp<EbookResp> list =ebookService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+}

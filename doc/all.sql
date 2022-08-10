9bc8fb45c5911a51e49978113444d6095b9d829a
xienima <2055472756@qq.com>
2022-08-11 00:12:02 +0800

    8-10 文档内容的保存与显示

--- doc/all.sql
+++ doc/all.sql
@@ -83,3 +83,23 @@ insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) val
 insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (4, 1, 3, '文档2.1', 1, 0, 0);
 insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, '文档2.2', 2, 0, 0);
 insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, '文档2.2.1', 1, 0, 0);
+
+-- 文档内容
+drop table if exists `content`;
+create table `content` (
+                           `id` bigint not null comment '文档id',
+                           `content` mediumtext not null comment '内容',
+                           primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='文档内容';
+
+-- 用户表
+drop table if exists `user`;
+create table `user` (
+                        `id` bigint not null comment 'ID',
+                        `login_name` varchar(50) not null comment '登陆名',
+                        `name` varchar(50) comment '昵称',
+                        `password` char(32) not null comment '密码',
+                        primary key (`id`),
+                        unique key `login_name_unique` (`login_name`)
+) engine=innodb default charset=utf8mb4 comment='用户';
+insert into `user` (id, login_name, name, password) VALUES (3,'test','测试','test');
